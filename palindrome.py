d8e7454b2feb17751bfecc65f539a8a441630063
chapi1420 <nadewnahom8@gmail.com>
2025-04-02 15:18:04 +0300

    check for palindrome of an integer given

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        x = str(x)
+        n = len(x)
+        for i in range(n // 2):
+            if x[i] != x[n - i - 1]:
+                return False
+        return True
